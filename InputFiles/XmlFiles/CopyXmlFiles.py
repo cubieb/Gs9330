edb9d66aeda98620f9aa5ead9c7a635c4c06ef35
cd01220 <10861479@qq.com>
2016-03-04 15:26:20 +0800

    merge from ver.3.0

--- InputFiles/XmlFiles/CopyXmlFiles.py
+++ InputFiles/XmlFiles/CopyXmlFiles.py
@@ -4,10 +4,12 @@ import glob
 import time
 
 regex="1_[0-9][0-9][0-9]_??t*.xml"
+
+os.renames("ok", "notok")
 for file in glob.glob(regex):
 	os.remove(file)
 	
 for file in glob.glob(os.path.join("../TestFile/RightXml", regex)):
 	shutil.copy(file, ".")
-
+os.renames("notok", "ok")
    
--- InputFiles/XmlFiles/DeleteXmlFiles.py
+++ InputFiles/XmlFiles/DeleteXmlFiles.py
@@ -4,7 +4,9 @@ import glob
 import time
 
 regex="1_[0-9][0-9][0-9]_??t*.xml"
+
+os.renames("ok", "notok")
 for file in glob.glob(regex):
 	os.remove(file)
-	
+os.renames("notok", "ok")	
    
